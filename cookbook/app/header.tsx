380babf0c9e013aa210cb6275d7f949056fd69aa
Andrew <andrewalthoff@outlook.com>
2025-12-06 20:09:24 -0600

    header added with color need to fix centering and spacing

--- cookbook/app/header.tsx
+++ cookbook/app/header.tsx
@@ -2,15 +2,31 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="w-full p-4 bg-red-600 text-white flex justify-between items-center">
-      <h1 className="text-2xl font-bold">CookBook</h1>
-      <nav>
-        <Link href="/" className="mr-4 hover:underline">
+    <header className="app-header mx-auto flex items-center px-8 py-2 h-32 border-b-4 bg-white shadow-md flex justify-between">
+      <h1 className="title">CookBook</h1>
+      <nav className="text-lg">
+        <div className="ce space-x-4">
+        <Link href="/" className="">
           Home
         </Link>
-        <Link href="/recipes" className="hover:underline">
+        <Link href="/about" className="">
+        About
+        </Link>
+        <Link href="/newRecipe" className="">
+          New Recipe
+        </Link>
+        <Link href="/recipes" className="">
           Recipes
         </Link>
+        </div>
+        <div className= "ml-8 flex space-x-4">
+          <Link href="/signup" className="">
+            Sign Up
+          </Link>
+          <Link href="/login" className="">
+            Log In
+          </Link>
+        </div>
       </nav>
     </header>
   );
